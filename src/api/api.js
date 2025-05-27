97a4848eab8e72a1f3cff2ed405c41d92dae14e3
abdojat <3oooo3.2003@gmail.com>
2025-05-27 11:33:34 +0300

    backend connection fixed

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api', 
+    baseURL: 'https://fbcloneapi-e7tl.onrender.com/api', 
 });
 
 API.interceptors.request.use((config) => {
