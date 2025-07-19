4dce5c296e920f2eb799a16d182b348b0fc297a5
abdojat <3oooo3.2003@gmail.com>
2025-07-20 00:13:54 +0300

    fixed loading protected routes

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -1,18 +1,24 @@
 // /src/routes/PrivateRoute.js
 
 import { Navigate, Outlet } from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
+import { Box, CircularProgress } from "@mui/material";
 
 const PrivateRoute = () => {
     const { user, loading } = useAuth();
-    const location = useLocation();
 
-    if (!user) {
-        return <Navigate to="/login" state={{ from: location }} replace />;
+    if (loading) {
+        return <Box sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh' // Full viewport height
+        }}>
+            <CircularProgress size={60} />
+        </Box>;
     }
-
-    return <Outlet />;
+    return user ? <Outlet /> : <Navigate to="/login" />;
+    
 };
 
 export default PrivateRoute;
